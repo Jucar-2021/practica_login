9a74e547c0bf4e413d3aadc106de1d4e6e31bb79
pruenas <92juancarlos@gmail.com>
2025-02-14 20:59:14 -0600

    se hace uso de addListener pra que escuche lo que se este escribiendo y de resultados al momento de la busqueda, de este modo se elimnima el boton buscar

--- lib/PaginaBusqueda.dart
+++ lib/PaginaBusqueda.dart
@@ -12,36 +12,54 @@ class Paginabusqueda extends StatefulWidget {
 
 class _PaginabusquedaState extends State<Paginabusqueda> {
   List<Map<String, String>> librosEncontrados = [];
-  final formKey = GlobalKey<FormState>();
   final TextEditingController ctrlBuscar = TextEditingController();
   final DatabaseReference db = FirebaseDatabase.instance.ref().child("libro");
 
+  @override
+  void initState() {
+    super.initState();
+    ctrlBuscar.addListener(buscarLibro); // Escucha cambios en el campo
+  }
+
+  @override
+  void dispose() {
+    ctrlBuscar.removeListener(buscarLibro);
+    ctrlBuscar.dispose();
+    super.dispose();
+  }
+
   Future<void> buscarLibro() async {
-    if (formKey.currentState?.validate() ?? false) {
-      String tituloABuscar = ctrlBuscar.text.toLowerCase();
-      DatabaseEvent event = await db.once();
-      DataSnapshot resultadosDB = event.snapshot;
+    String tituloABuscar = ctrlBuscar.text.toLowerCase().trim();
 
-      List<Map<String, String>> resultados = [];
-
-      for (var libro in resultadosDB.children) {
-        String titulo = libro.child("titulo").value.toString().toLowerCase();
-        if (titulo.contains(tituloABuscar)) {
-          resultados.add({
-            "titulo": libro.child("titulo").value.toString(),
-            "imagen": libro.child("imagen").value?.toString() ?? "",
-            "autor": libro.child("autor").value.toString(),
-            "editorial": libro.child("editorial").value.toString(),
-            "paginas": libro.child("paginas").value.toString(),
-            "genero": libro.child("genero").value.toString(),
-          });
-        }
-      }
-
+    if (tituloABuscar.isEmpty) {
       setState(() {
-        librosEncontrados = resultados;
+        librosEncontrados = [];
       });
+      return;
     }
+
+    DatabaseEvent event = await db.once();
+    DataSnapshot resultadosDB = event.snapshot;
+
+    List<Map<String, String>> resultados = [];
+
+    for (var libro in resultadosDB.children) {
+      String titulo = libro.child("titulo").value.toString().toLowerCase();
+      if (titulo.contains(tituloABuscar)) {
+        resultados.add({
+          "titulo": libro.child("titulo").value.toString(),
+          "imagen": libro.child("imagen").value?.toString() ?? "",
+          "autor": libro.child("autor").value.toString(),
+          "editorial": libro.child("editorial").value.toString(),
+          "paginas": libro.child("paginas").value.toString(),
+          "genero": libro.child("genero").value.toString(),
+        });
+      }
+    }
+
+    setState(() {
+      librosEncontrados = resultados;
+    });
   }
 
   @override
@@ -57,99 +75,88 @@ class _PaginabusquedaState extends State<Paginabusqueda> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Form(
-              key: formKey,
-              child: Column(
-                children: [
-                  TextFormField(
-                    controller: ctrlBuscar,
-                    validator: (value) => value!.isEmpty ? "Campo Vacío" : null,
-                    decoration: InputDecoration(
-                      labelText: "Libro",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(25.0),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  ElevatedButton.icon(
-                    onPressed: buscarLibro,
-                    icon: const Icon(Icons.search),
-                    label: const Text("Buscar"),
-                  ),
-                ],
+            TextFormField(
+              controller: ctrlBuscar,
+              decoration: InputDecoration(
+                labelText: "Buscar Libro",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
+                prefixIcon: const Icon(Icons.search),
               ),
+              onChanged: (value) {
+                buscarLibro();
+              },
             ),
             const SizedBox(height: 20),
             Expanded(
               child: librosEncontrados.isEmpty
                   ? const Center(child: Text("No se encontraron resultados"))
                   : ListView.builder(
-                      itemCount: librosEncontrados.length,
-                      itemBuilder: (context, index) {
-                        final libro = librosEncontrados[index];
-                        return GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => Paginadetalles(
-                                  libro: librosEncontrados[0],
-                                ),
-                              ),
-                            );
-                          },
-                          child: Card(
-                            margin: const EdgeInsets.symmetric(vertical: 8),
-                            elevation: 4,
-                            child: Padding(
-                              padding: const EdgeInsets.all(12.0),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Container(
-                                    height: 200,
-                                    width: 120,
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(10),
-                                      boxShadow: [
-                                        BoxShadow(
-                                          color: Colors.black12,
-                                          blurRadius: 6,
-                                          offset: Offset(0, 3),
-                                        ),
-                                      ],
-                                    ),
-                                    child: ClipRRect(
-                                      borderRadius: BorderRadius.circular(10),
-                                      child: libro["imagen"]!.isNotEmpty
-                                          ? Image.memory(
-                                              base64Decode(libro["imagen"]!),
-                                              fit: BoxFit.contain,
-                                            )
-                                          : const Icon(
-                                              Icons.image_not_supported,
-                                              size: 80,
-                                              color: Colors.grey,
-                                            ),
-                                    ),
-                                  ),
-                                  const SizedBox(height: 10),
-                                  Text(
-                                    libro["titulo"]!,
-                                    style: const TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 18,
-                                    ),
-                                    textAlign: TextAlign.center,
+                itemCount: librosEncontrados.length,
+                itemBuilder: (context, index) {
+                  final libro = librosEncontrados[index];
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              Paginadetalles(libro: libro),
+                        ),
+                      );
+                    },
+                    child: Card(
+                      margin: const EdgeInsets.symmetric(vertical: 8),
+                      elevation: 4,
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Container(
+                              height: 200,
+                              width: 120,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                boxShadow: const [
+                                  BoxShadow(
+                                    color: Colors.black12,
+                                    blurRadius: 6,
+                                    offset: Offset(0, 3),
                                   ),
                                 ],
                               ),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: libro["imagen"]!.isNotEmpty
+                                    ? Image.memory(
+                                  base64Decode(libro["imagen"]!),
+                                  fit: BoxFit.contain,
+                                )
+                                    : const Icon(
+                                  Icons.image_not_supported,
+                                  size: 80,
+                                  color: Colors.grey,
+                                ),
+                              ),
                             ),
-                          ),
-                        );
-                      },
+                            const SizedBox(height: 10),
+                            Text(
+                              libro["titulo"]!,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
+                  );
+                },
+              ),
             ),
           ],
         ),
