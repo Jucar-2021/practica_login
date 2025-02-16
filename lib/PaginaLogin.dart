43e1f313bed1c329842c934440d9c1937462ac9a
pruenas <92juancarlos@gmail.com>
2025-02-16 01:56:47 -0600

    ajustes

--- lib/PaginaLogin.dart
+++ lib/PaginaLogin.dart
@@ -1,17 +1,21 @@
+import 'package:flutter/material.dart';
 import 'google_signin.dart';
 import 'libros_firebase.dart';
-import 'package:flutter/material.dart';
 
-class Paginalogin extends StatelessWidget {
+class Paginalogin extends StatefulWidget {
   const Paginalogin({super.key});
 
   @override
+  _PaginaloginState createState() => _PaginaloginState();
+}
+
+class _PaginaloginState extends State<Paginalogin> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text("Login"),
+        title: Text("Valida tu cuanta"),
         centerTitle: true,
         backgroundColor: Colors.amber,
       ),
@@ -20,6 +24,7 @@ class Paginalogin extends StatelessWidget {
           width: 300,
           child: ElevatedButton(
             onPressed: () {
+              setState(() {}); // Forzar la reconstrucción
               iniciarConGoogle().whenComplete(() => Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context) => LibrosFirebase()),
@@ -32,7 +37,7 @@ class Paginalogin extends StatelessWidget {
                   "assets/logoGo.gif",
                   scale: 4,
                   errorBuilder: (context, error, stackTrace) {
-                    return Icon(Icons.error, color: Colors.red); // Si hay error, muestra un ícono
+                    return Icon(Icons.error, color: Colors.red);
                   },
                 ),
                 const SizedBox(width: 10),
@@ -42,7 +47,7 @@ class Paginalogin extends StatelessWidget {
                 ),
               ],
             ),
-          )
+          ),
         ),
       ),
     );
